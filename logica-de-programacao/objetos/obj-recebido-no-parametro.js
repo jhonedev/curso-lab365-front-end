09d42c095bd1416934bc75276fcc59344f72cc98
Jonathan dos Santos Borges <jonathanborges@MacBook-Air-de-Jonathan.local>
2025-10-25 12:39:25 -0300

    Exercicios sobre objetos em js

---
+++ logica-de-programacao/objetos/obj-recebido-no-parametro.js
@@ -0,0 +1,29 @@
+function objetoNoLog(obj) {
+    console.log(obj)
+}
+
+// Não modifique as linhas abaixo
+let pessoa = {
+    nome: "Samuel",
+    sobrenome: "Duarte",
+    idade: 21,
+    email: "samuelduarte@email.com",
+    estaVerificado: true
+};
+
+let config = {
+    tema: "escuro",
+    moeda: "real"
+};
+
+objetoNoLog(pessoa);
+objetoNoLog(config);
+
+// Criar objeto restaurante
+let restaurante = {
+  nome: "barucPizzas",
+  classificacao: 5,
+  abertoNasSegundas: true
+}
+
+console.log(restaurante)

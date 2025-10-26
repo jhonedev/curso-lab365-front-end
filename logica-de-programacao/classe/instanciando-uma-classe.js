8861c954ae46b1b9eff180c8eb7ca7f1e5b6cc6c
Jonathan dos Santos Borges <jonathanborges@MacBook-Air-de-Jonathan.local>
2025-10-26 17:02:37 -0300

    Instancias da classes

---
+++ logica-de-programacao/classe/instanciando-uma-classe.js
@@ -0,0 +1,26 @@
+class Usuario {
+    // classe vazia
+}
+// instancie o objeto aqui
+let usuario = new Usuario();
+// Digite seu código abaixo
+let usuario1 = new Usuario();
+let usuario2 = new Usuario();
+
+// Digite seu código aqui
+let macarrao = new Receita();
+let salada = new Receita();
+
+// Não mexa no código abaixo
+// Definição da classe 
+class Receita {
+    // classe vazia
+}
+
+// definição da classe
+class Receita{
+
+}
+
+// instanciação da classe 
+let receita = new Receita();

a8a3c9ec3321e4bff86ca6562b15ea9ae2cad0ff
Jonathan dos Santos Borges <jonathanborges@MacBook-Air-de-Jonathan.local>
2025-10-28 13:32:48 -0300

    implementacao dos metodos da classe usuario

--- logica-de-programacao/classe/implementando-metodos-de-instancia.js
+++ logica-de-programacao/classe/implementando-metodos-de-instancia.js
@@ -32,3 +32,62 @@ class Carro {
         return `Este carro de modelo ${this.modelo} é do ano de ${this.ano}`
     }
 }
+
+// Definição da classe
+class Receita {
+    constructor(nome, calorias) {
+        this.nome = nome;
+        this.calorias = calorias;
+    }
+    
+    // Implemente os métodos de instância ehBaixaCaloria() e ehAltaCaloria()
+    ehBaixaCaloria(){
+        return this.calorias <= 400;
+    }
+
+    ehAltaCaloria(){
+        return this.calorias >= 600
+    }
+}
+
+// Uso da classe
+let macarrao = new Receita("Macarrão", 700);
+console.log(macarrao.ehBaixaCaloria());
+console.log(macarrao.ehAltaCaloria());
+
+let salada = new Receita("Salada", 350);
+console.log(salada.ehBaixaCaloria());
+console.log(salada.ehAltaCaloria());
+
+// Definição da classe
+class Usuario {
+    /* 
+        Implemente os métodos: 
+        1. constructor 
+        2. obterNomeCompleto 
+        3. obterIniciais 
+        4. podeVotar
+    */
+    constructor(nome, sobrenome, idade){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+
+    obterNomeCompleto(){
+        return `${this.nome} ${this.sobrenome}`
+    }
+
+    obterIniciais(){
+        return `${this.nome[0]}${this.sobrenome[0]}`
+    }
+
+    podeVotar(){
+        return this.idade >= 16;
+    }
+}
+
+// Uso da classe
+let sara = new Usuario("Sara", "Gomes", 17);
+let cesar = new Usuario("César", "Assis", 13);
+console.log("---");

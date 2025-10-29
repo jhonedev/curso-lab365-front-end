d980b8e13b510f8f8130dabd517a827f301565f9
JhoneDev <jonatan.borges2012@hotmail.com>
2025-10-29 22:39:56 +0000

    implemenando metodos de instancia

--- logica-de-programacao/classe/implementando-metodos-de-instancia.js
+++ logica-de-programacao/classe/implementando-metodos-de-instancia.js
@@ -20,6 +20,27 @@ let usuario1 = new Usuario("Jonathan", "Borges");
 
 usuario1.obterNomeCompleto();
 
+// Definição da classe
+class User {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    // Implemente o método obterNomeCompleto()
+    obterNomeCompleto(){
+        return `${this.nome} ${this.sobrenome}`
+    }
+}
+
+// Uso da classe
+let samuel = new User("Samuel", "Bueno");
+console.log(samuel.obterNomeCompleto());
+
+let carla = new User("Carla", "Marins");
+console.log(carla.obterNomeCompleto());
+
+console.log("---");
 
 class Carro {
     constructor (modelo, ano, marca){

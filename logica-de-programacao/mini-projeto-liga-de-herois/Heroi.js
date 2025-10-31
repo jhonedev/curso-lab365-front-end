0a2cf40aedd2bcd6413514403181b9e70264285f
Jonathan dos Santos Borges <jonathanborges@MacBook-Air-de-Jonathan.local>
2025-10-31 13:59:22 -0300

    implementando os metodos calcularmediapontos e classificar da classe heroi

--- logica-de-programacao/mini-projeto-liga-de-herois/Heroi.js
+++ logica-de-programacao/mini-projeto-liga-de-herois/Heroi.js
@@ -4,6 +4,44 @@ export class Heroi extends Personagem {
     constructor(nome, poder, pontos) {
         //super
         super(nome, poder)
-        this.pontos = pontos; // somente os herois que tem os pontos
+        this.pontos = pontos; // somente os herois que tem os pontos ex: [1,2,3]
     }
+
+    //calcularMediaPontos
+    calcularMediaPontos(){
+        //ordenar
+        let pontosOrdenados = []
+        //sort
+        if(this.pontos){ // se existir pelo menos 1 ponto entra na condicao para receber os pontos ordenados
+            pontosOrdenados = this.pontos.sort(function(a, b){ //ordenando os numeros com o sort
+                return a - b
+            });
+        }
+        //5 - fatiar a lista
+        //slice
+        let pontosValidos = pontosOrdenados.slice(0, 5) //pegando 5 primeiros numeros da lista dos pontos ordenados
+        let soma = pontosValidos.reduce(function(total, ponto){
+            return total + ponto
+        }, 0)
+        
+        let media = soma / pontosValidos.length
+
+        return media
+    }
+
+    //classificar
+    classificar(){
+        let media = this.calcularMediaPontos();
+
+        if (media <= 30){
+            return "Iniciante"
+        }
+
+        if (media <= 70){
+            return "Intermediario"
+        }
+
+        return "Lendario"
+    }
+    //getResumo
 }
--- logica-de-programacao/mini-projeto-liga-de-herois/index.js
+++ logica-de-programacao/mini-projeto-liga-de-herois/index.js
@@ -3,14 +3,21 @@
 //missao
 //liga
 //import, export...
-
-//import { Personagem } from "./Personagem.js";
 import { Heroi } from "./Heroi.js";
 
 let batman = new Heroi("Batman",
     "Filantropia noturna e planejamento exagerado.",
-    [8, 10, 9, 7, 10]
-); // classe (model) => objeto
+    [8, 10, 9, 31, 7, 10, 1, 2, 21]
+);
 
-console.log(batman)
-console.log(batman.getInfo())
+let flash = new Heroi("Flash",
+    "Entrega antes do prazo",
+    [60, 80, 90, 95, 100])
+
+//console.log(batman)
+//console.log(batman.getInfo()) // suas informacoes
+console.log(batman.calcularMediaPontos()) //mostra a media
+console.log(batman.classificar()) //mostra sua classificacao +> iniciante, intermediario ou lendario
+
+console.log(flash.calcularMediaPontos())
+console.log(flash.classificar())
